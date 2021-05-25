d19915b1a68da3e70c50dcfbf82ce628150b65d4
山田 健太 <32208@ojs.ac.jp>
2021-05-25 10:37:56 +0900

    P54リスト2.9 静的クラスへ変更

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 namespace DistanceConverter
 {
     //フィートとメートルの単位変換クラス
-    class FeetConverter
+    public static class FeetConverter
     {
-        public double FromMeter(double meter)
+        public static double FromMeter(double meter)
         {
             //メートルからフィートを求める
             return meter / 0.3048;
         }
 
         //フィートからメートルを求める
-        public double ToMeter(double feet)
+        public static  double ToMeter(double feet)
         {
             return feet * 0.3048;
         }
--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -22,21 +22,21 @@ namespace DistanceConverter
 
         private static void PrintMeterToFeetList(int start,int stop)
         {
-            FeetConverter converter = new FeetConverter();
+            
             for (int meter = start; meter <= stop; meter++)
             {
-                double feet = converter.ToMeter(meter);
+                double feet = FeetConverter.ToMeter(meter);
                 Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
         }
 
         private static void PrintFeetToMeterList(int start ,int stop)
         {
-            FeetConverter converter = new FeetConverter();
+            
             //フィートからメートルへの対応表を出力
             for (int feet = start; feet <= stop; feet++)
             {
-                double meter = converter.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
 
             }
