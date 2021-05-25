fba6049e7d53caf1e799b85cd240120dbd8a7ece
山田 健太 <32208@ojs.ac.jp>
2021-05-25 11:34:23 +0900

    P５９リスト2.14 売上クラス作成

---
+++ SalesCalculator/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator
+{
+    class Sale
+    { //売上クラス
+        
+        public string ShopName { get; set; }
+        //店舗名
+
+        public string ProductCategory { get; set; }
+        //商品カテゴリ
+
+        public int Amount { get; set; }
+        //売上高
+    }
+}
