a3f65891bb7025b6e8f51a1375e3cc383daefc3b
山田 健太 <32208@ojs.ac.jp>
2021-06-01 10:25:42 +0900

    p79 問題　2.2 完成

---
+++ Excise2/InchConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excise2
+
+{
+    //フィートとメートルの単位変換クラス
+    public static class InchConverter
+    {
+        private const double ratio = 0.0254;
+
+        public static double FromMeter(double meter)
+        {
+            //メートルからインチを求める
+            return meter / ratio;
+        }
+
+        //インチからメートルを求める
+        public static  double ToMeter(double feet)
+        {
+            return feet * ratio;
+        }
+    }
+}
--- Excise2/Program.cs
+++ Excise2/Program.cs
@@ -10,7 +10,37 @@ namespace Excise2
     {
         static void Main(string[] args)
         {
+            if (args.Length >= 1 && args[0] == "-toi")
+            {
+                //インチからメートルへの対応表を出力
+                PrintMeterToInchList(1, 10);
+            }
+            else
+            {
+                //インチからメートルへの対応力表を出力
+                PrintInchToMeterList(1, 10);
+            }
 
+        }
+
+        private static void PrintInchToMeterList(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double Inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} Inch", meter, Inch);
+            }
+        }
+
+        private static void PrintMeterToInchList(int start, int stop)
+        {
+            //フィートからメートルへの対応表を出力
+            for (int Inch = start; Inch <= stop; Inch++)
+            {
+                double meter = InchConverter.FromMeter(Inch);
+                Console.WriteLine("{0} Inch = {1:0.0000} m", Inch, meter);
+
+            }
         }
     }
 }
