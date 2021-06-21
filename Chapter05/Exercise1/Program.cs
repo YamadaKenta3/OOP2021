9d5d33591ad0f2547e70307a02292f97dc9a659f
山田 健太 <32208@ojs.ac.jp>
2021-06-21 13:25:19 +0900

    一期目試験

---
+++ Chapter05/Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+#if false
+            #region
+            Console.WriteLine("入力してください");
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+
+            if (string.Compare(s1, s2, ignoreCase:true) == 0)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
+       
+        }
+            #endregion
+#endif
+        }
+
+    }
+}
