0ac8a3f8c61073039175bf6dce50906d2900c68f
健太 山田 <32208@ojs.ac.jp>
2021-11-12 13:16:24 +0900

    SampleUnitConverter 完成

---
+++ WPF/SampleUnitConverter/DistanceUnit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter
+{
+    public class DistanceUnit
+    {
+        public string Name { get; set; }
+        public double Coefficient { get; set; }
+        public override string ToString()
+        {
+            return this.Name;
+
+        }
+
+    }
+
+
+    public class MetricUnit : DistanceUnit
+    {
+        private static List<MetricUnit> units = new List<MetricUnit>
+        {
+            new MetricUnit{Name = "mm", Coefficient = 1, },
+            new MetricUnit{Name = "cm", Coefficient = 10, },
+            new MetricUnit{Name = "m", Coefficient = 10 * 100, },
+            new MetricUnit{Name = "km", Coefficient = 10 * 100 * 1000, },
+
+        };
+        public static ICollection<MetricUnit> Units { get { return units; } }
+    
+        /// <summary>
+        /// ヤード単位からメートル単位に変換します
+        /// </summary>
+        /// <param name="unit">インチ単位</param>
+        /// <param name="value">係数</param>
+        /// <returns></returns>
+        public double FromImperialUnit(ImperialUnit unit, double value)
+        {
+            return (value * unit.Coefficient) * 25.4 / this.Coefficient;
+        }
+    }
+
+
+
+
+    public class ImperialUnit : DistanceUnit
+    {
+        private static List<ImperialUnit> units = new List<ImperialUnit>
+        {
+            new ImperialUnit{Name = "in", Coefficient = 1, },
+            new ImperialUnit{Name = "ft", Coefficient = 12, },
+            new ImperialUnit{Name = "yd", Coefficient = 12 * 3, },
+            new ImperialUnit{Name = "ml", Coefficient = 12 * 3 * 1760, },
+        };
+        public static ICollection<ImperialUnit> Units { get { return units; } }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="unit">メートル単位</param>
+        /// <param name="value">係数</param>
+        /// <returns></returns>
+        public double FromMetricUnit(MetricUnit unit, double value)
+        {
+            return (value * unit.Coefficient) * 25.4 / this.Coefficient;
+        }
+
+    }
+
+  }
+
+
+
+
