481d2d9cfa2b72e793e3bb6238147051b393e0b9
健太 山田 <32208@ojs.ac.jp>
2021-11-12 15:56:51 +0900

    Namber Game 途中

---
+++ WPF/NamberGame/RandamNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamberGame
+{
+    class RandamNumber
+    {
+        public int Number { get; set; }
+        
+    }
+
+    public class RandomNum
+    {
+
+        int seikai = new Random().Next(1, 25);
+      
+
+    }
+}
