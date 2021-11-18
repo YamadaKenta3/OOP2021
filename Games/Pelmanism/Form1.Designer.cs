50e9056cb81048c8a35264059c21fb281e940fb3
健太 山田 <32208@ojs.ac.jp>
2021-11-18 11:44:06 +0900

    神経衰弱　画面完成

---
+++ Games/Pelmanism/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Pelmanism
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelGuidance = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelGuidance
+            // 
+            this.labelGuidance.AutoSize = true;
+            this.labelGuidance.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelGuidance.ForeColor = System.Drawing.Color.Red;
+            this.labelGuidance.Location = new System.Drawing.Point(99, 9);
+            this.labelGuidance.Name = "labelGuidance";
+            this.labelGuidance.Size = new System.Drawing.Size(41, 12);
+            this.labelGuidance.TabIndex = 0;
+            this.labelGuidance.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0秒経過";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "スタート";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 311);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelGuidance);
+            this.Name = "FormGame";
+            this.Tag = "475,350";
+            this.Text = "神経衰弱";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelGuidance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Games/Pelmanism/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pelmanism
+{
+    class Player
+    {
+        private int nowOpenCardIndex1; //今回一枚目に開いたカードの添え字
+        private int nowOpenCardIndex2; //前回二枚目に開いたカードの添え字
+
+        //前回1枚目に開いたカードの添え字
+        public int BeforeOpenCardIndex1 { get; set; }
+        //前回2枚目に開いたカードの添え字
+        public int BeforeOpenCardIndex2 { get; set; }
+
+        //めくった枚数
+        public int OpenCounter { get; set; }
+
+        public Player()
+        {
+            nowOpenCardIndex1 = nowOpenCardIndex2 = -1;
+            BeforeOpenCardIndex1 = BeforeOpenCardIndex2 = -1;
+            OpenCounter = 0;
+        }
+        //今回１枚目に開いたカードの添え字
+        public int NowOpenCardIndex1
+        {
+            get { return nowOpenCardIndex1; }
+            set
+            {
+                nowOpenCardIndex1 = value;
+                OpenCounter++;
+            }
+        }
+        public int NowOpenCardIndex2
+        {
+            get { return nowOpenCardIndex2; }
+            set
+            {
+                nowOpenCardIndex2 = value;
+                OpenCounter++;
+            }
+        }
+        //カードの情報をリセット
+        public void Reset()
+        {
+            BeforeOpenCardIndex1 = NowOpenCardIndex1;
+            BeforeOpenCardIndex2 = NowOpenCardIndex2;
+            nowOpenCardIndex1 = -1;
+            NowOpenCardIndex2 = -1;
+            OpenCounter = 0;
+        }
+    }
+}
